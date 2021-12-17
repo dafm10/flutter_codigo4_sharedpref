fbe4dbc6c9869da7d571bf8055d09660d210c765
dafm10 <dafm.10@gmail.com>
2021-12-16 19:11:22 -0500

    Settings Page, saveDataRegister

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({Key? key}) : super(key: key);
@@ -12,6 +13,29 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   bool darkMode = false;
   int gender = 1;
+  String fullName = "";
+  String address = "";
+
+  // siempre que usemos inputs debemos controlarlos
+  TextEditingController fullNameControlled = TextEditingController();
+  TextEditingController addressControlled = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  saveDataRegister() async {
+    SharedPreferences _data = await SharedPreferences.getInstance();
+
+    _data.setString("fullName", fullNameControlled.text);
+    _data.setString("address", addressControlled.text);
+    _data.setBool("darkMode", darkMode);
+    _data.setInt("gender", gender);
+
+    print("Guardando datos...");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -86,104 +110,103 @@ class _RegisterPageState extends State<RegisterPage> {
           ],
         ),
       ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              "Settings Page",
-              style: TextStyle(
-                fontSize: 30.0,
-                fontWeight: FontWeight.w700,
-                color: Color(0xff424242),
-              ),
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                hintText: "Full name",
-              ),
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                hintText: "Address",
-              ),
-            ),
-            //Switch(value: true, onChanged: (bool value){}),
-            SwitchListTile(
-              value: darkMode,
-              title: Text("Darl Mode"),
-              onChanged: (bool value) {
-                darkMode = value;
-                setState(() {});
-              },
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            const Text(
-              "Gender",
-              style: TextStyle(
-                fontSize: 18.0,
-              ),
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            RadioListTile(
-              value: 1,
-              groupValue: gender,
-              title: Text("Male"),
-              onChanged: (int? value) {
-                gender = value!;
-                setState(() {});
-              },
-            ),
-            RadioListTile(
-              value: 2,
-              groupValue: gender,
-              title: Text("Female"),
-              onChanged: (int? value) {
-                gender = value!;
-                setState(() {});
-              },
-            ),
-            RadioListTile(
-              value: 3,
-              groupValue: gender,
-              title: Text("No definido"),
-              onChanged: (int? value) {
-                gender = value!;
-                setState(() {});
-              },
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Container(
-              width: double.infinity,
-              height: 45.0,
-              child: ElevatedButton.icon(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.black87,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Settings Page",
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.w700,
+                  color: Color(0xff424242),
                 ),
-                onPressed: () {},
-                icon: Icon(Icons.save),
-                label: Text(
-                  "Save",
-                  style: TextStyle(
-                    fontSize: 18.0,
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              TextField(
+                controller: fullNameControlled,
+                decoration: InputDecoration(
+                  hintText: "Full name",
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              TextField(
+                controller: addressControlled,
+                decoration: InputDecoration(
+                  hintText: "Address",
+                ),
+              ),
+              //Switch(value: true, onChanged: (bool value){}),
+              SwitchListTile(
+                value: darkMode,
+                title: Text("Darl Mode"),
+                onChanged: (bool value) {
+                  darkMode = value;
+                  setState(() {});
+                },
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              const Text(
+                "Gender",
+                style: TextStyle(
+                  fontSize: 18.0,
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              RadioListTile(
+                value: 1,
+                groupValue: gender,
+                title: Text("Male"),
+                onChanged: (int? value) {
+                  gender = value!;
+                  setState(() {});
+                },
+              ),
+              RadioListTile(
+                value: 2,
+                groupValue: gender,
+                title: Text("Female"),
+                onChanged: (int? value) {
+                  gender = value!;
+                  setState(() {});
+                },
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                width: double.infinity,
+                height: 45.0,
+                child: ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.black87,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      saveDataRegister();
+                    });
+                  },
+                  icon: Icon(Icons.save),
+                  label: Text(
+                    "Save",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
